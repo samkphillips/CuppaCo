9cadb71664cbd673c7f4d7a81dfd2cf4c6d58182
Sam <sam.k.phillips@gmail.com>
2021-09-02 16:48:19 -0400

    Blocked out about us

--- main/client/src/pages/AboutUs.jsx
+++ main/client/src/pages/AboutUs.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    //
-    <h1>About Us Page!</h1>
+    <div className='about-page'>
+      <h1>About Us</h1>
+      <h2>Image of the Cuppa Co team (me, lol)</h2>
+      <p>Here at Cuppa Co, we believe that coffee is good and also you should drink it and talk about it. We have a passion for talking about coffee and making money off of people talking about coffee, probably.</p>
+      <h3>Cute Logo</h3>
+    </div>
   )
 }
 
