ce1c7671203a5b1952faabe4c47588965b2ba444
Sam <sam.k.phillips@gmail.com>
2021-09-02 12:56:32 -0400

    blocked out the home page

--- main/client/src/App.js
+++ main/client/src/App.js
@@ -1,12 +1,21 @@
 import { Route, Switch } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <h1>Howdy</h1>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path='/roasts' component={} />
+          <Route exact path='/shop' component={} />
+          <Route exact path='/aboutus' component={} /> */}
+        </Switch>
+      </main>
     </div>
   )
 }
