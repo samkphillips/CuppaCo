ac681965c98b1ed1912377e0d02cf26c1aa453be
Sam <sam.k.phillips@gmail.com>
2021-09-02 12:48:56 -0400

    Added Header component

---
+++ main/client/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <NavLink to="/" className="navlinks">
+          Home
+        </NavLink>
+        <NavLink to="/roasts" className="navlinks">
+          Roasts
+        </NavLink>
+        <NavLink to="/shop" className="navlinks">
+          Shop
+        </NavLink>
+        <NavLink to="/aboutus" className="navlinks">
+          About Us
+        </NavLink>
+      </nav>
+    </header>
+  )
+}
