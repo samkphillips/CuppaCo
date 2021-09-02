bd3ea5adb991cc10d5d44d2ab9118a434cd49f82
Sam <sam.k.phillips@gmail.com>
2021-09-02 17:01:40 -0400

    Blocking out roasts page

--- main/client/src/pages/Roasts.jsx
+++ main/client/src/pages/Roasts.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Roasts = () => {
   return (
-    //
-    <h1>Roasts Page!</h1>
+    <div className='roasts-page'>
+      <div className='roasts-sidebar'>
+        <p>Link 1</p>
+        <p>Link 2</p>
+      </div>
+      <div className='roasts-container'>
+        {/* roast cards live here */}
+        <h1>Roasts!!!</h1>
+      </div>
+    </div>
   )
 }
 
