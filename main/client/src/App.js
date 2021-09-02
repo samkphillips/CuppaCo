a9e959de7bb3bececac85523d447830fe5ae0f67
Sam <sam.k.phillips@gmail.com>
2021-09-02 12:52:05 -0400

    Header shows up

--- main/client/src/App.js
+++ main/client/src/App.js
@@ -1,8 +1,11 @@
+import { Route, Switch } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Howdy</h1>
     </div>
   )
