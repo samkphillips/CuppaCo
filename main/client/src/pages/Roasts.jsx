73353812981fba96e7cd4d690e345ec520797241
Sam <sam.k.phillips@gmail.com>
2021-09-03 03:29:36 -0400

    Trimmed searching roasts, don't have time

--- main/client/src/pages/Roasts.jsx
+++ main/client/src/pages/Roasts.jsx
@@ -11,15 +11,11 @@ const Roasts = () => {
       <div className='roasts-sidebar'>
         <NavLink to={`${url}`} className="navlinks">Roasts</NavLink>
         <NavLink to={`${url}/add`} className="navlinks">Add Roast</NavLink>
-        <NavLink to={`${url}/search`} className="navlinks">Search</NavLink>
       </div>
       
       <Switch>
         <Route exact path={path} component={AllRoasts} />
         <Route path={`${path}/add`} component={AddRoast} />
-        <Route path={`${path}/search`}>
-          <h1>Search Roast</h1>
-        </Route>
       </Switch>
     </div>
   )
