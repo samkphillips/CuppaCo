5a3ccfe4b44728cf87e0cb62469195874bede768
Sam <sam.k.phillips@gmail.com>
2021-09-05 20:44:03 -0400

    Setting up Heroku deploy

--- main/client/src/globals.js
+++ main/client/src/globals.js
@@ -1,1 +1,4 @@
-export const BASE_URL = 'http://localhost:3001/api'
+export const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? `${window.location.origin}/api`
+    : 'http://localhost:3001/api'
